b284de6566235b52038b60ce8e364367aa33a0c9
Jaspirr <jesper.erlandsson@hotmail.com>
2022-10-16 21:51:35 +0200

    added error message

--- script.js
+++ script.js
@@ -1,31 +1,44 @@
-const checkLength = (element, minLength = 2) => {
-    console.log(element.target.id)
+const checkLength = (element, minLength = 2, message) => {
+    
+    if(message === undefined)
+        message = `Your ${element.target.id} must have at least ${minLength} characters`
 
     if (element.target.value.length < minLength) {
-        document.getElementById(`${element.target.id}ErrorMessage`).innerText = "You must enter a name"
+        document.getElementById(element.target.id).classList.add('error')
+        document.getElementById(`${element.target.id}ErrorMessage`).innerHTML = message
     } else {
+        document.getElementById(element.target.id).classList.remove('error')
         document.getElementById(`${element.target.id}ErrorMessage`).innerText = ""
     }
 }
 
+const checkEmail = (element, message) => {
 
+    if(message === undefined)
+        message = `Your ${element.target.id} must be a valid e-mail address`
 
-
-
+    if (!element.target.value.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)){
+        document.getElementById(element.target.id).classList.add('error')
+        document.getElementById(`${element.target.id}ErrorMessage`).innerHTML = message
+    } else {
+        document.getElementById(element.target.id).classList.remove('error')
+        document.getElementById(`${element.target.id}ErrorMessage`).innerText = ""
+    }
+}
 
 const validate = (e) => {
     switch(e.target.type) {
         case "text":
-            checkLength(e)
+            checkLength(e, 2)
             break;
         case "email":
+            checkEmail(e)
             break;
         case "textarea":
+            checkLength(e, 5)
             break;
     }
 }
-
-
 
 
 
