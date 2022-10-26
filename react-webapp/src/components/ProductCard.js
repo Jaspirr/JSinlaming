663ffcf1e496b62520ef4dcbe04b5988c5948f66
Jaspirr <jesper.erlandsson@hotmail.com>
2022-10-26 18:58:15 +0200

    added cards

--- react-webapp/src/components/ProductCard.js
+++ react-webapp/src/components/ProductCard.js
@@ -1,27 +1,58 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import TempImg from '../assets/images/img1.jpg'
 
-const PoductCard = () => {
+const PoductCard = ({item}) => {
   return (
+    <div className="col" >
       <div className="card">
-        <ul className="card-menu">
-            <li id="heart"><a href="#"><i className="fa-regular fa-heart"></i></a></li>
-            <li id="shuffel"><a href="#"><i className="fa-regular fa-shuffle"></i></a></li>
-            <li id="bag"><a href="#"><i className="fa-regular fa-bag-shopping"></i></a></li>
-        </ul>
+        <div className="card-img">
+          <img src={`../assets/images/${item.img}`} alt={item.name}></img>
+          <div className="card-menu">
+            <button className="menu-link"><i className="fa-regular fa-heart"></i></button>
+            <button className="menu-link"><i className="fa-regular fa-code-compare"></i></button>
+            <button className="menu-link"><i className="fa-regular fa-bag-shopping"></i></button>
+          </div>
+          <NavLink to="/products" className="btn-theme btn-card-theme">
+            <span className="corner-left"></span>
+            <span className="corner-right"></span>
+            QUICK VIEW
+          </NavLink>
+        </div>
         <div className="card-body">
-            <button id="btn-card" className="btn-theme">QUICK VIEW</button>
+          <p className="card-category">{item.category}</p>
+          <h5 className="card-title">{item.name}</h5>
+          <p className="card-rating">
+            <i className="fa-sharp fa-solid fa-star"></i>
+            <i className="fa-sharp fa-solid fa-star"></i>
+            <i className="fa-sharp fa-solid fa-star"></i>
+            <i className="fa-sharp fa-solid fa-star"></i>
+            <i className="fa-sharp fa-solid fa-star"></i>
+          </p>
+          <p className="card-price">{item.price}</p>
         </div>
-        <div className="category-titel">category</div>
-        <div className="category-name">Modern Black Blouse</div>
-        <div className="stars">
-            <i className="fa-sharp fa-solid fa-star-sharp"></i>
-            <i className="fa-sharp fa-solid fa-star-sharp"></i>
-            <i className="fa-sharp fa-solid fa-star-sharp"></i>
-            <i className="fa-sharp fa-solid fa-star-sharp"></i>
-            <i className="fa-sharp fa-solid fa-star-sharp"></i>
-        </div>
-        <div className="price">$35.00</div>    
       </div>
+    </div>
+      // <div className="card">
+      //   <ul className="card-menu">
+      //       <li id="heart"><a href="#"><i className="fa-regular fa-heart"></i></a></li>
+      //       <li id="shuffel"><a href="#"><i className="fa-regular fa-shuffle"></i></a></li>
+      //       <li id="bag"><a href="#"><i className="fa-regular fa-bag-shopping"></i></a></li>
+      //   </ul>
+      //   <div className="card-body">
+      //       <button id="btn-card" className="btn-theme">QUICK VIEW</button>
+      //   </div>
+      //   <div className="category-titel">category</div>
+      //   <div className="category-name">Modern Black Blouse</div>
+      //   <div className="stars">
+      //       <i className="fa-sharp fa-solid fa-star-sharp"></i>
+      //       <i className="fa-sharp fa-solid fa-star-sharp"></i>
+      //       <i className="fa-sharp fa-solid fa-star-sharp"></i>
+      //       <i className="fa-sharp fa-solid fa-star-sharp"></i>
+      //       <i className="fa-sharp fa-solid fa-star-sharp"></i>
+      //   </div>
+      //   <div className="price">$35.00</div>    
+      // </div>
   )
 }
 
--- react-webapp/src/sections/ProductGridSection.js
+++ react-webapp/src/sections/ProductGridSection.js
@@ -1,17 +1,25 @@
-import React from 'react'
-import ProductCard from '../components/ProductCard';
+import React, { useState } from 'react'
+import ProductCard from '../components/ProductCard'
 
-const ProductGridSection = () => {
+const ProductGridSection = ({title}) => {
+
+  const [products, setProducts] = useState([
+    { id: 1, name : "modern Black Blouse", category: "Fashion", price: "$35.00", rating: 5, img: "img1.jpg"},
+    { id: 2, name : "modern Black Blouse", category: "Fashion", price: "$35.00", rating: 5, img: "img1.jpg"},
+    { id: 3, name : "modern Black Blouse", category: "Fashion", price: "$35.00", rating: 5, img: "img1.jpg"},
+    { id: 4, name : "modern Black Blouse", category: "Fashion", price: "$35.00", rating: 5, img: "img1.jpg"}
+  ])
+
   return (
-    <section classNameName="product-grid">
-        <div classNameName="featured-titel">Featured Product</div>
-            <div className="container">
-                <div className="parent">
-                        <div id="grid-1" className="grid">
-                            <ProductCard />
-                        </div>
-                </div>
-            </div>
+    <section className="product-grid">
+      <div className="container">
+        <h1 className="featured-title">{title}</h1>
+        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-4">
+            {
+              products.map(product => <ProductCard item={product} />)
+            }
+        </div>
+      </div>
     </section>
   )
 }
