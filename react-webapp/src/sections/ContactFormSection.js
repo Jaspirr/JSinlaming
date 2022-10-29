d6c97e7348f85ee16547d9308517ce8ea6dbd613
Jaspirr <jesper.erlandsson@hotmail.com>
2022-10-29 15:48:14 +0200

    added onChange

--- react-webapp/src/sections/ContactFormSection.js
+++ react-webapp/src/sections/ContactFormSection.js
@@ -1,30 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const ContactFormSection = () => {
-  return (
-    <section className="contact-form">
-        <div className="container">
-            <h2>Come In Contact With Us</h2>
-            <form noValidate>
-                <div>
-                    <input id="name" type="text" placeholder="Your Name" />
-                    <div className="errorMessage"></div>
-                </div>
-                <div>
-                    <input id="email" type="email" placeholder="Your Mail" />
-                    <div className="errorMessage"></div>
-                </div>
-                <div className="textarea">
-                    <textarea id="comment" placeholder="Comments"></textarea>
-                    <div className="errorMessage"></div>
-                </div>
-                <div className="formBtn">
-                    <button type="submit" className="btn-theme">Post Comments</button>
-                </div>
-            </form>
-        </div>
-    </section>
-  )
+    const [contactForm, setContactForm] = useState ({name: '', email: '', comment: ''})
+
+    const handleChange = (e) => {
+        const {id, value} = e.target
+        setContactForm ({...contactForm, [id]: value})
+    }
+    return (
+        <section className="contact-form">
+            <div className="container">
+                <h2>Come In Contact With Us</h2>
+                <form onSubmit="submitForm" noValidate>
+                    <div>
+                        <input id="name" type="text" placeholder="Your Name" value={contactForm.name} onChange={handleChange}/>
+                        <div id="emailErrorMessage" className="errorMessage"></div>
+                    </div>
+                    <div>
+                        <input id="email" onKeyUp="validate(event)" type="email" placeholder="Your Mail" value={contactForm.email} onChange={handleChange}/>
+                        <div id="emailErrorMessage" className="errorMessage"></div>
+                    </div>
+                    <div className="textarea">
+                        <textarea id="message" placeholder="Comments" value={contactForm.comment} onChange={handleChange}></textarea>
+                        <div id="messageErrorMessage" className="errorMessage"></div>
+                    </div>
+                    <div className="formBtn">
+                        <button id="send-button" type="submit" className="btn-theme">Post Comments</button>
+                    </div>
+                </form>
+            </div>
+        </section>
+    )
 }
 
 export default ContactFormSection
